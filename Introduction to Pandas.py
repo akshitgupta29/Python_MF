08c8a0bc6c7e4584d9740b3051f7b9d6d2af6c73
Akshit Gupta <akshitgupta29@gmail.com>
2019-05-29 15:41:31 +0530

    Introduction to Pandas

--- Introduction to Pandas.py
+++ Introduction to Pandas.py
@@ -11,8 +11,9 @@
 
 """
 
-import numpy as np
 import pandas as pd
+import numpy as np
+
 
 s = pd.Series ([1,2,3,np.nan, 5,6], index = ['a', 'b', 'c','c', 'e', 'f'])
 
@@ -28,5 +29,69 @@ df = pd.DataFrame (data, columns = ['Emp_ID', 'Gender', 'Age'])
 print (df)
 df
 
+#Concat or append operation
+data = {
+'emp_id': ['1', '2', '3', '4', '5'],
+'first_name': ['Jason', 'Andy', 'Allen', 'Alice', 'Amy'],
+'last_name': ['Larkin', 'Jacob', 'A', 'AA', 'Jackson']}
+df_1 = pd.DataFrame(data, columns = ['emp_id', 'first_name', 'last_name'])
+data = {
+'emp_id': ['4', '5', '6', '7'],
+'first_name': ['Brian', 'Shize', 'Kim', 'Jose'],
+'last_name': ['Alexander', 'Suma', 'Mike', 'G']}
+df_2 = pd.DataFrame(data, columns = ['emp_id', 'first_name', 'last_name'])
 
+df_1
+df_2
+
+# Usingconcat
+df = pd.concat([df_1, df_2])
+df
+#The other way
+df_1.append(df_2)
+
+#Concatenation on the basis of columns
+pd.concat([df_1, df_2], axis=1)
+
+#Merge two dataframes
+# Merge two dataframes based on the emp_id value
+# in this case only the emp_id's present in both table will be joined
+pd.merge(df_1,df_2, on='emp_id')
+
+#Join
+"""
+Pandas offer SQL style merges as well.
+Left join produces a complete set of records from Table A, with the matching records
+where available in Table B. If there is no match, the right side will contain null.
+"""
+
+#Left join two dataframes
+# Left join
+pd.merge(df_1, df_2, on='emp_id', how='left')
+# Merge while adding a suffix to duplicate column names of both table
+print(pd.merge(df_1, df_2, on='emp_id', how='left', suffixes=('_left', '_right')))
+
+
+"""
+Right join - Right join produces a complete set of records from Table B,
+with the matching records where available in Table A. If there is no match,
+the left side will contain null.
+"""
+
+# Right join
+print(pd.merge(df_1, df_2, on='emp_id', how='right'))
+
+"""
+Inner Join - Inner join produces only the set of records that match in both Table A
+and Table B.
+"""
+print(pd.merge(df_1, df_2, on='emp_id', how='inner'))
+
+
+"""
+Outer Join - Full outer join produces the set of all records in Table A and Table B, with
+matching records from both sides where available. If there is no match, the missing side
+will contain null
+"""
+print(pd.merge(df_1, df_2, on='emp_id', how='outer'))
 
