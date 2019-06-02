14e47fbc17181d49ae676700a8c51933b4d2993c
Akshit Gupta <akshitgupta29@gmail.com>
2019-06-02 23:40:52 +0530

    Adding the images  and the dummy coding

---
+++ Pandas/DummyCoding.py
@@ -0,0 +1,13 @@
+"""
+Introduction to Dummy Coding and Image Handling.
+Images for reference for Dummy coding are opened in the code first.
+
+@Author: Akshit Gupta
+"""
+
+#To open a image
+from PIL import Image
+img1 = Image.open("C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Images\\Dummy_Coding_1a.jpg").show()
+img2 = Image.open("C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Images\\Dummy_Coding_1b.jpg").show()
+img3 = Image.open("C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Images\\Dummy_Coding_1c.jpg").show()
+
--- Pandas/Handling NaN and Duplicates.py
+++ Pandas/Handling NaN and Duplicates.py
@@ -34,4 +34,69 @@ crops[crops.cost.notnull()].Crop #shows the rows without NAs in crops.Crop
 
 #To remove NaNs if any of 'Yield' or'cost' are missing
 #use the subset parameter and pass a list:
-a = crops.dropna()
+a = crops.dropna(subset=['Yeild','cost'], how='any')
+a
+crops
+b = crops.dropna()
+b
+c = crops.dropna(subset=["Yeild",'cost'], how='all')
+c
+
+#Dealing with duplicates
+data = pd.DataFrame({"Items" : ["TV","Washing Machine","Mobile","TV","TV","Washing Machine"], 
+                     "Price" : [10000,50000,20000,10000,10000,40000]})
+data
+data2 = data.copy()
+data2
+
+"""
+pandas.DataFrame.duplicated
+
+Return boolean Series denoting duplicate rows, optionally only considering certain columns.
+Parameters:	
+subset : column label or sequence of labels, optional
+Only consider certain columns for identifying duplicates, by default use all of the columns
+
+keep : {‘first’, ‘last’, False}, default ‘first’
+first : Mark duplicates as True except for the first occurrence.
+last : Mark duplicates as True except for the last occurrence.
+False : Mark all duplicates as True.
+
+Returns:	
+duplicated : Series
+
+"""
+
+#duplicated() returns a logical vector returning True 
+#when encounters duplicated.
+data.duplicated()
+data.duplicated().sum() #number of duplicatees in the data.
+data.loc[data.duplicated(),:]
+
+#By default keep = 'first' i.e. the first occurence is considered a unique 
+#value and its repetitions are considered as duplicates.
+#If keep = "last" the last occurence is considered a unique value 
+#and all its repetitions are considered as duplicates.
+
+#last entries are not there,indices have changed.
+data.loc[data.duplicated(keep="last"),:]
+
+#If keep = "False" then it considers all the occurences of the repeated 
+#observations as duplicates.
+
+#all the duplicates, including unique are shown.
+data.loc[data.duplicated(keep=False)]
+
+#To drop the duplicates drop_duplicates is used 
+#with default inplace = False, keep = 'first' or 'last' 
+#or 'False' have the respective meanings as in duplicated( )
+data.drop_duplicates(keep = "first")
+data.drop_duplicates(keep = "last")
+data.drop_duplicates(keep=False, inplace=True) #by default inplace = False
+data
+data2.drop_duplicates(keep=False)
+data2
+
+
+
+
