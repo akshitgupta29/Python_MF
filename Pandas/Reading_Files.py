8f5ce9729b532ddd3697cc086f73b17bb794cd48
Akshit Gupta <akshitgupta29@gmail.com>
2019-06-13 17:48:54 +0530

    Reading_Files_updated

--- Pandas/Reading_Files.py
+++ Pandas/Reading_Files.py
@@ -8,8 +8,10 @@ Exploring the various arguments present under the read functions().
 import pandas as pd
 import numpy as np
 
-pd.read_csv('C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Data\\titanic.csv')
+titanic = pd.read_csv('C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Data\\titanic.csv')
+titanic
 
 #Taking the index column as pclass.
 pd.read_csv('C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Data\\titanic.csv', index_col='pclass')
 
+pd.read_csv('C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Data\\titanic.csv', header=0)
