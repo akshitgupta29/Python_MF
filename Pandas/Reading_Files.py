885a277d378499dc674011137f3884dde96090e2
Akshit Gupta <akshitgupta29@gmail.com>
2019-06-14 11:22:27 +0530

    adding the reading_files

--- Pandas/Reading_Files.py
+++ Pandas/Reading_Files.py
@@ -14,4 +14,19 @@ titanic
 #Taking the index column as pclass.
 pd.read_csv('C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Data\\titanic.csv', index_col='pclass')
 
-pd.read_csv('C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Data\\titanic.csv', header=0)
+#To make the second row of a csv file as a header. By default the first row is treated as header.
+pd.read_csv('C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Data\\titanic.csv', header=1)
+
+#To have no header and taking the first row in csv as the first row only.
+pd.read_csv('C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Data\\titanic.csv', header=None)
+
+#To show the columns in the order supplied.
+pd.read_csv('C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Data\\titanic.csv', header=0, names=["alive","class","gender", "age","sibsp","parch","price","emb","deck"])
+
+#Don't try this. Fully distorted.
+pd.read_csv('C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Data\\titanic.csv', header=0, names=["alive","class","gender", "age","sibsp","parch"])
+
+#To use only specific columns from the csv.
+pd.read_csv('C:\\Users\\guptaksh.CORPDOM\\Desktop\\Self_Learning\\MF_Python\\Self_code\\Pandas\\Data\\titanic.csv', header=0, usecols=["survived", "pclass", "sex", "age"])
+
+
